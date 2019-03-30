1ed09bcb895b3c00f3821655a710fb84aa5acc76
TejinderKaur <kaurtejinder31095@gmail.com>
2019-03-30 08:17:17 -0400

    added required pages for Angular CRUD and security

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,10 +16,15 @@ import { ServicesComponent } from './pages/services/services.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
+import { ContactDeleteComponent } from './contacts/contact-delete/contact-delete.component';
 
 // services
 import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages';
-import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+
 
 @NgModule({
   declarations: [
@@ -33,7 +38,11 @@ import { ContactListComponent } from './contacts/contact-list/contact-list.compo
     ContactComponent,
     HomeComponent,
     PageNotFoundComponent,
-    ContactListComponent
+    ContactListComponent,
+    RegisterComponent,
+    LoginComponent,
+    ContactDetailsComponent,
+    ContactDeleteComponent
   ],
   imports: [
     BrowserModule,
